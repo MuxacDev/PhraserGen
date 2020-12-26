fc0b4a6255fa266d341d1e47c83f8c275fc3833e
Mikhail Buimister <elevis567@gmail.com>
2020-12-26 16:38:50 +0700

    Запуск приложения в режиме отладки

--- app/src/main/java/com/example/phrasergen/MainActivity.java
+++ app/src/main/java/com/example/phrasergen/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.phrasergen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Gen.setOnClickListener(onClickPhrase);
     }
+
+    public void onClickPhrase(View view) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Все, что нам нужно - это "+Phraser.PhraserGen());
+    }
+
 }
+
+
+
